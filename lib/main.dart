a71077c35ba643c793e19496609652a676ed6cca
kokoriru <yuka@ryukalice.com>
2021-11-15 15:21:21 +0900

    Tween の animate メソッドに AnimationController を渡す形式でサイズ可変正方形のアニメーションを実装

--- lib/main.dart
+++ lib/main.dart
@@ -33,6 +33,11 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
   late AnimationController _animationControler;
+  late Animation<double> _animationDouble;
+  Tween<double> _tweenDouble = Tween(begin: 0.0, end: 200.0);
+  late Animation<Color?> _animationColor;
+  ColorTween _tweenColor = ColorTween(begin: Colors.green, end: Colors.blue);
+
   _play() async {
     setState(() {
       _animationControler.forward();
@@ -56,6 +61,15 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
     super.initState();
     _animationControler =
         AnimationController(vsync: this, duration: Duration(seconds: 3));
+
+    _animationDouble = _tweenDouble.animate(_animationControler);
+    _animationDouble.addListener(() {
+      setState(() {});
+    });
+    _animationColor = _tweenColor.animate(_animationControler);
+    _animationColor.addListener(() {
+      setState(() {});
+    });
   }
 
   @override
@@ -74,15 +88,16 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Text("AnimationController:${_animationControler.value}"),
+            Text("AnimationDouble:${_animationDouble.value}"),
+            Text("AnimationColor:${_animationColor.value}"),
             SizeTransition(
               sizeFactor: _animationControler,
               child: Center(
                 child: SizedBox(
-                  width: 50,
-                  height: 50,
-                  child: Container(color: Colors.green)
-                )
-              ),
+                  width: _animationDouble.value,
+                  height: _animationDouble.value,
+                  child: Container(color: _animationColor.value))),
             ),
           ],
         ),
@@ -90,13 +105,11 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
       floatingActionButton:
         Row(mainAxisAlignment: MainAxisAlignment.center, children: [
           FloatingActionButton(
-            onPressed: _play, child: Icon(Icons.arrow_forward)
-          ),
+            onPressed: _play, child: Icon(Icons.arrow_forward)),
           FloatingActionButton(onPressed: _stop, child: Icon(Icons.pause)),
           FloatingActionButton(
-            onPressed: _reverse, child: Icon(Icons.arrow_back)
-          ),
-        ],
+            onPressed: _reverse, child: Icon(Icons.arrow_back)),
+        ]
       ),
     );
   }
